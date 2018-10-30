2913b84d335a8f24ea0db043423e7602b4261bf5
Stopa42 <honza.stopka@gmail.com>
2018-10-31 00:34:52 +0100

    DANumber structure and basic operations

--- src/DifferentialAlgebra.jl
+++ src/DifferentialAlgebra.jl
@@ -1,5 +1,109 @@
 module DifferentialAlgebra
 
-    
+    export DANumber
 
+    struct DANumber{T,N} <: Number
+        order::Integer
+        dimension::Integer
+        danumber::NTuple{N,T}
+        function DANumber{T,N}(o,d,da) where {T,N}
+            if o < 0
+                throw(ArgumentError("Order of DANumber must be non-negative."))
+            elseif d < 1
+                throw(ArgumentError("Dimension of DANumber must be positive."))
+            else
+                N2 = binomial(o+d,d)
+                if N == N2
+                    return new(o,d,da)
+                else
+                    throw(ArgumentError("Order and dimension must match ntuple."))
+                end
+            end
+        end
+    end
+
+    function DANumber(o,d,da)
+        T = typeof(first(da))
+        l = length(da)
+        lnew = binomial(o+d,d)
+        danew = Tuple((i > l ? zero(T) : da[i] for i = 1:lnew))
+        return DANumber{T,lnew}(o,d,danew)
+    end
+
+    ord(a::DANumber) = a.order
+
+    dim(a::DANumber) = a.dimension
+
+    tuple(a::DANumber) = a.danumber
+
+    import Base.+
+
+    +(a::Real,b::DANumber) = DANumber(ord(a),dim(a),Tuple(())
+
+    function +(a::DANumber,b::DANumber)
+        if dim(a) == dim(b)
+            oa, ob = ord(a), ord(b)
+            daa, dab = tuple(a), tuple(b)
+            na, nb = length(daa), length(dab)
+            T = promote_type(typeof(first(daa)),typeof(first(dab)))
+            d = dim(a)
+            if oa > ob
+                return DANumber(oa,d,Tuple((daa[i] + (i > nb ? zero(T) : dab[i]) for i = 1:na)))
+            else
+                return DANumber(ob,d,Tuple((dab[i] + (i > na ? zero(T) : daa[i]) for i = 1:nb)))
+            end
+        else
+            throw(MethodError("Cannot add DANumbers of different dimensions."))
+        end
+    end
+
+    function +(a::DANumber{T,N},b::DANumber{S,N}) where {T,S,N}
+        if dim(a) == dim(b)
+            daa, dab = tuple(a), tuple(b)
+            o = ord(a)
+            d = dim(a)
+            return DANumber(o,d,Tuple((daa[i] + dab[i] for i = 1:N)))
+        else
+            throw(MethodError("Cannot add DANumbers of different dimensions."))
+        end
+    end
+
+    import Base.-
+
+    function -(a::DANumber,b::DANumber)
+        if dim(a) == dim(b)
+            oa, ob = ord(a), ord(b)
+            daa, dab = tuple(a), tuple(b)
+            na, nb = length(daa), length(dab)
+            T = promote_type(typeof(first(daa)),typeof(first(dab)))
+            d = dim(a)
+            if oa > ob
+                return DANumber(oa,d,Tuple((daa[i] - (i > nb ? zero(T) : dab[i]) for i = 1:na)))
+            else
+                return DANumber(ob,d,Tuple(((i > na ? zero(T) : daa[i]) - dab[i] for i = 1:nb)))
+            end
+        else
+            throw(MethodError("Cannot subtract DANumbers of different dimensions."))
+        end
+    end
+
+    function -(a::DANumber{T,N},b::DANumber{S,N}) where {T,S,N}
+        if dim(a) == dim(b)
+            daa, dab = tuple(a), tuple(b)
+            o = ord(a)
+            d = dim(a)
+            return DANumber(o,d,Tuple((daa[i] - dab[i] for i = 1:N)))
+        else
+            throw(MethodError("Cannot subtract DANumbers of different dimensions."))
+        end
+    end
+
+    import Base.*
+
+    *(a::Real,b::DANumber) = DANumber(ord(b),dim(b),Tuple((a*bval for bval in tuple(b))))
+    *(b::DANumber,a::Real) = a*b
+
+    import Base.<
+
+    <(a)
 end # End of module
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,3 +2,56 @@ using Test
 using DifferentialAlgebra
 
 @test 1+1==2
+
+@testset "Constructors" begin
+    a = @test_nowarn DANumber(1,1,(1.0,0.0))
+    @test a isa Number
+    b = @test_nowarn DANumber(1,1,(1.0,))
+    @test b isa Number
+    @test b == a
+    c = @test_nowarn DANumber(1,1,(1.0,0.0,1.0,1.0))
+    @test c isa Number
+    @test c == b
+end
+
+@testset "Addition" begin
+    a = DANumber(1,1,(1.0,2.0))
+    b = DANumber(1,1,(2.0,3.0))
+    c = DANumber(1,1,(3.0,5.0))
+    @test a + b == c
+
+    a = DANumber(2,1,(1.0,2.0,5.0))
+    b = DANumber(1,1,(2.0,3.0))
+    c = DANumber(2,1,(3.0,5.0,5.0))
+    @test a + b == c
+
+    a = DANumber(1,1,(1.0,2.0))
+    b = DANumber(2,1,(2.0,3.0,5.0))
+    c = DANumber(2,1,(3.0,5.0,5.0))
+    @test a + b == c
+end
+
+@testset "Subtraction" begin
+    a = DANumber(1,1,(1.0,2.0))
+    b = DANumber(1,1,(2.0,3.0))
+    c = DANumber(1,1,(-1.0,-1.0))
+    @test a - b == c
+
+    a = DANumber(2,1,(1.0,2.0,5.0))
+    b = DANumber(1,1,(2.0,3.0))
+    c = DANumber(2,1,(-1.0,-1.0,5.0))
+    @test a - b == c
+
+    a = DANumber(1,1,(1.0,2.0))
+    b = DANumber(2,1,(2.0,3.0,5.0))
+    c = DANumber(2,1,(-1.0,-1.0,-5.0))
+    @test a - b == c
+end
+
+@testset "Scalar multiplication" begin
+    a = 2
+    b = DANumber(1,1,(1.0,2.5))
+    c = DANumber(1,1,(2.0,5.0))
+    @test a*b == c
+    @test b*a == c
+end
